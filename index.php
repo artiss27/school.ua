2df23d47c4163494867056b17c0e6c24f7e6a669
Манаенко Руслан Валериевич <Mon@MacBook-Pro-ARTiss.local>
2015-11-10 13:27:03 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=utf-8');
+//session_start();
+?>
+<html>
+<header>
+    <script src="/vendor/public/jquery/dist/jquery.min.js"></script>
+</header>
+<body>
+<div>начальная тестовая страница</div>
+</body>
+</html>
