f629c18db289bacb4789dd835f28e2f248f1bbe1
artiss27 <0227@i.ua>
2015-11-19 12:15:03 +0200

    new version

--- 10.php
+++ 10.php
@@ -22,7 +22,6 @@
     <script src="/vendor/public/jquery/dist/jquery.min.js"></script>
 </head>
 <body>
-
 <header>
     <div class="top">
         <a class="logo" href="/"><img src="/img/6/logo.png" alt="логотип"></a>
@@ -78,108 +77,99 @@
         </select>
     </nav>
 </header>
-
 <main>
     <div class="banner">
         <img src="/img/6/central.jpg" alt="слайдер">
     </div>
+    <div class="sect_1">
+        <article class="info_3">
+            <img src="/img/6/page-img.png" alt="кукуруза" class="round">
+
+            <h2>New<br>
+                Technologies</h2>
+
+            <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum
+                delenitimos.</p>
+            <a href="#"><span class="button"></span></a>
+        </article>
+        <article class="info_3">
+            <img src="/img/6/page-img-1.png" alt="огурцы" class="round">
+
+            <h2>Frost<br>
+                protection</h2>
+
+            <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum
+                delenitimos.</p>
+            <a href="#"><span class="button"></span></a>
+        </article>
+        <article class="info_3">
+            <img src="/img/6/page-img-2.png" alt="редиска" class="round">
+
+            <h2>eco<br>
+                solutions</h2>
+
+            <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum
+                delenitimos.</p>
+            <a href="#"><span class="button"></span></a>
+        </article>
+    </div>
+
+
+
+
+
+    <div class="line"></div>
+    <div class="sect_2">
+        <section class="info_3">
+            <p class="h3_b">welcome!</p>
+            <img src="/img/6/page-img-3.png" alt="Приветствие">
+
+            <p>Ut vero eos et accusamus et iusto odio dignissimos ducimus qui voluptatum deleniti atque corrupti quos
+                dolores et quasmolestias exceptu. </p>
+            <a href="#"><span class="button"></span></a>
+        </section>
+        <section class="info_3">
+            <p class="h3_b">our mission</p>
+
+            <p class="col_bg">lorem ipsum dolore massa as laoreet magna aliqua</p>
+
+            <p class="dark">Ut vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent.</p>
+
+            <p>Eluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
+                provident similique <a href="#">sunt in culpa qui</a> officia deserunt mollitia animi, id est laborum et
+                dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
+                nobis est eligendi opticumque </p>
+            <a href="#"><span class="button"></span></a>
+        </section>
+        <section class="info_3">
+            <div class="info_small">
+                <div class="circle">01.</div>
+                <div class="big">vegetable seeds</div>
+            </div>
+            <p>Lorem ipsum dolor sit ctetueradipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolorese
+                magna aiqu. </p>
+
+            <div class="info_small">
+                <div class="circle">02.</div>
+                <div class="big">agricultural seeds</div>
+            </div>
+            <p>Lorem ipsum dolor sit ctetueradipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolorese
+                magna aiqu. </p>
+            <a href="#"><span class="button"></span></a>
+        </section>
+    </div>
 </main>
-
 <footer>
-<address>
-</address>
+    <p>copyright</p>
+    <span>&copy 2013  | <a href="#">Privacy Policy</a></span>
+
+    <address>
+        <p>address</p>
+        <span>9870 St Vincent Place, Glasgow, DC 45 Fr 45.</span>
+    </address>
+
+    <p>Follow Us</p>
+    <span>&nbsp;</span>
 </footer>
-
-
-<!--
-<div class="main">
-  <table class="header content">
-    <tr>
-      <th><a href="/"><img src="/img/6/logo.png" alt="логотип"></a></th>
-      <td class="dark">HAVE AN ACCOUNT? <a href="#">SIGN IN</a> OR <a href="#">SIGN UP</a></td>
-    </tr>
-  </table>
-  <table class="navigation content">
-    <tr>
-      <td><a href="#">HOME</a></td>
-      <td><a href="#">ABOUT</a></td>
-      <td><a href="#">PRODUCTS</a></td>
-      <td><a href="#">SERVICES</a></td>
-      <td><a href="#">BLOG</a></td>
-      <td><a href="#">CONTACTS</a></td>
-    </tr>
-  </table>
-  <table class="banner content">
-    <tr>
-      <td><img src="/img/6/central.jpg" alt="слайдер"></td>
-    </tr>
-  </table>
-  <table class="section content">
-    <tr>
-      <td><img src="/img/6/page-img.png" alt="кукуруза" class="round">
-        <h2>New<br>
-          Technologies</h2>
-        <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum delenitimos.</p>
-      <td><img src="/img/6/page-img-1.png" alt="огурцы" class="round">
-        <h2>Frost<br>
-          protection</h2>
-        <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum delenitimos.</p>
-      <td><img src="/img/6/page-img-2.png" alt="редиска" class="round">
-        <h2>eco<br>
-          solutions</h2>
-        <p>At vero eos et accusamus et iusto ssimos ducimus qui blanditiistes es praesentiumvoluptatum delenitimos.</p>
-    </tr>
-    <tr>
-      <td><a href="#"><span class="button"></span></a></td>
-      <td><a href="#"><span class="button"></span></a></td>
-      <td><a href="#"><span class="button"></span></a></td>
-    </tr>
-  </table>
-  <table class="line content">
-  </table>
-
-
-  <table class="info content">
-    <tr>
-      <td class="col"><p class="h3_b">welcome!</p>
-        <img src="/img/6/page-img-3.png" alt="Приветствие">
-        <p>Ut vero eos et accusamus et iusto odio dignissimos ducimus qui voluptatum deleniti atque corrupti quos dolores et quasmolestias exceptu. </p></td>
-      <td class="col"><p class="h3_b">our mission</p>
-        <p class="col_bg">lorem ipsum dolore massa as laoreet magna aliqua</p>
-        <p class="dark">Ut vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent.</p>
-        <p>Eluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident similique <a href="#">sunt in culpa qui</a> officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi opticumque </p></td>
-      <td class="col"><table class="info_small">
-          <tr>
-            <td class="circle">01.</td>
-            <td class="big">vegetable seeds</td>
-          </tr>
-        </table>
-        <p>Lorem ipsum dolor sit ctetueradipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolorese magna aiqu. </p>
-        <table class="info_small">
-          <tr>
-            <td class="circle">02.</td>
-            <td class="big">agricultural seeds</td>
-          </tr>
-        </table>
-        <p>Lorem ipsum dolor sit ctetueradipiscing elit. Sed diam nonummy nibh euismod tincidunt ut laoreet dolorese magna aiqu. </p></td>
-    </tr>
-    <tr>
-      <td><a href="#"><span class="button"></span></a></td>
-      <td><a href="#"><span class="button"></span></a></td>
-      <td><a href="#"><span class="button"></span></a></td>
-    </tr>
-  </table>
-    <table class="footer_info content">
-          <tr>
-            <td><p>copyright</p>
-              <span>&copy 2013  | <a href="#">Privacy Policy</a></span></td>
-            <td><p>address</p>
-              <span>9870 St Vincent Place, Glasgow, DC 45 Fr 45.</span></td>
-            <td><p>Follow Us</p>
-              <span>&nbsp;</span></td>
-          </tr>
-    </table>
-</div>
--->
 </body>
 </html>
