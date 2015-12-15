62d6310c2acd4f6b7af185d53e3f1268f35a58f4
artiss27 <0227@i.ua>
2015-12-15 14:22:42 +0200

    new version

--- index.php
+++ index.php
@@ -4,19 +4,19 @@ ini_set('display_errors', 1);
 header('Content-Type: text/html; charset=utf-8');
 //session_start();
 ?>
-</body>
-</html>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Заголовок страницы</title>
-    <meta name="apple-mobile-web-app-title" content="Заголовок без SEO">
-    <meta name="description" content="Описание страницы">
-    <meta name="keywords" content="Ключевые слова для некоторых поисковиков">
+    <title>Заказ музыкального коллектива на праздник в Москве и МО</title>
+    <!--<meta name="apple-mobile-web-app-title" content="Заголовок без SEO">-->
+    <meta name="description"
+          content="Подача заявки на заказ музыкального коллектива в Москве и МО для обслуживания праздников и корпоративов">
+    <meta name="keywords" content="заказ, музыкальный коллектив, проблемы, отзывы, вокальный дуэт">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="robots" content="index, follow">
+    <!--
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="apple-touch-icon" href="/touch-icon-iphone.png">
@@ -24,29 +24,294 @@ header('Content-Type: text/html; charset=utf-8');
     <link rel="apple-touch-icon" sizes="120x120" href="/touch-icon-iphone-retina.png">
     <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon-ipad-retina.png">
     <link href="/styles/style.css" rel="stylesheet">
+    -->
     <!--<script src="/scripts.js" defer></script>-->
     <script src="/vendor/public/jquery/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="/styles/flexslider/flexslider.css" type="text/css" media="screen"/>
 
 
 </head>
 <body>
 <header>
-    Логотип
-    <nav>
-        Основная навигация
-    </nav>
-    <!--<div class="mini menu" onclick="$('#menu').toggle('slow');">=</div>-->
-    <!--div.header>ul>li*5 далее ажимаем TAB-->
+    <a class="logo" href="/"><img src="/img/6/logo.png" alt="логотип"></a>
+    <h1>Музыкальный коллектив на праздник в Москве и МО</h1>
+    <div class="tel">+7(495)760-96-50 +7(495)586-80-03</div>
+    <div id="order_call">Закажите звонок</div>
 </header>
-
 <main>
+    <div class="center">
+        <h2>Профессиональный вокальный дуэт</h2>
+        <p>Светлана Чумакова и Константин Бовсуновский</p>
+        <p class="text_blue">Наш дуэт дополняют скрипка, саксофон, клавишные</p>
+        <div class="flexslider">
+            <ul class="slides">
+                <li>
+                    <img src="/img/6/slide.jpg" alt="">
+                </li>
+                <li>
+                    <img src="/img/6/slide-1.jpg" alt="">
+                </li>
+                <li>
+                    <img src="/img/6/slide-2.jpg" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="sidebar">
+        <form class="sidebar_blue" action="/">
+            <p>Оставьте заявку сейчас и получите скидку 20%</p>
+            <p class="text_blue">До конца акции:</p>
+            <input type="text" name="name" value="" placeholder="Ваше имя*">
+            <input type="tel" name="tel" value="" placeholder="Ваш телефон*">
+            <input type="email" name="email" value="" placeholder="Ваш e-mail*">
+            <input type="submit" value="ОСТАВИТЬ ЗАЯВКУ">
+        </form>
+    </div>
+    <div class="hr_block">
+        <div class="center_sidebar">
+            <p>С нами работают</p>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+        </div>
+    </div>
 
-    Основное содержание страницы
+    <div class="center">
+        <h3>Проблемы, с которыми сталкиваются 90% клиентов при заказе музыкального коллектива</h3>
+        <ul>
+            <li>
+                <p class="text_blue">Ценовая политика</p>
+                <p>Желание клиента сэкономить, зачастую оборачивается неприятными сюрпризами на мероприятии.</p>
+            </li>
+            <li>
+                <p class="text_blue">Не качественное исполнение</p>
+                <p>Не профессиональный вокал, не качественный звук, не презентабельный внешний вид.</p>
+            </li>
+            <li>
+                <p class="text_blue">Пустые слова</p>
+                <p>Музыканты не выполняют своих обещаний.</p>
+            </li>
+            <li>
+                <p class="text_blue">Одноплановый репертуар</p>
+                <p>Шаблоны по которым работают большинство коллективов.</p>
+            </li>
+            <li>
+                <p class="text_blue">Не умение музыкантов действовать по обстановке</p>
+                <p>Неумение быстро реагировать на разные меняющиеся ситуации на мероприятии.</p>
+            </li>
+        </ul>
+        <h3>Почему мы?</h3>
+        <ul>
+            <li>
+                <p class="text_blue">Большой разноплановый репертуар</p>
+                <p>В нашем репертуаре: песни 60-70х годов, диско 80х и хиты 90х, классика отечественной и зарубежной
+                    эстрады. Современные хиты и мы постоянно пополняем свой репертуар и учим новые песни по вашим
+                    заказам.</p>
+            </li>
+            <li>
+                <p class="text_blue">Красивое и душевное исполнение именно ваших любимых песен</p>
+                <p>Подготовка к каждому мероприятию, учитываем все предпочтения заказчика.</p>
+            </li>
+            <li>
+                <p class="text_blue">Профессиональное оборудование и качественный звук без переплат</p>
+                <p>Мы используем профессиональное звуковое и световое оборудование и не берем дополнительной платы за
+                    его аренду.</p>
+            </li>
+            <li>
+                <p class="text_blue">Интерактивная работа с гостями</p>
+            </li>
+            <li>
+                <p class="text_blue">Наш дуэт можно расширить, дополнительно пригласив- скрипку, саксофон, клавишные</p>
+            </li>
+            <li>
+                <p class="text_blue">Гонорар за наше выступление оговаривается индивидуально</p>
+                <p>Он зависит от ваших пожеланий и удаленности площадки на которой будет проходить мероприятие.</p>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar">
+        <h3>Отзывы</h3>
+        <div class="sidebar_blue">
+            <img src="/img/6/logo.png" alt="логотип">
+            <p class="rose">&laquo;Очень понравилось музыкальное сопровождение...&raquo;</p>
+            <p>
+                Светлана! Спасибо за проведенный Юбилей 20.04.2013 г. в ресторане &laquo;Пироговский дворик&raquo;!<br>
+                Очень понравилось музыкальное сопровождение &mdash; все песни в Вашем исполнении и в исполнении
+                Константина.<br>
+                Хорошо была организована программа вечера &mdash; поздравления, конкурсы, музыкальная костиная. Никто не
+                скучал. Праздник удался.
+            </p>
+            <p class="signature">Мамалльгина О.А.</p>
+        </div>
+        <div class="sidebar_blue">
+            <img src="/img/6/logo.png" alt="логотип">
+            <p class="rose">&laquo;...получили огромное удовольствие и массу позитивных эмоций&raquo;</p>
+            <p>
+                Коллектив компании ООО &laquo;Гоу-Лайф&raquo; выражает благодарность агенству &laquo;Vatel&raquo; за
+                отличную организацию и проведение новогоднего корпоративного праздника для сотрудников ООО &laquo;Гоу-Лайф&raquo;.<br>
+                Нам хотелось бы отдельно поблагодарить ведущую и певицу Светлану Чумакову за профессионализм,
+                креативность, отзывчивость и стремление сделать из любого мероприятия что-то особенное. Сотрудники нашей
+                компании получили огромное удовольствие и массу позитивных эмоций от организованного Вами вечера.
+            </p>
+            <p class="signature">Ген. директор ООО &laquo;Гоу-Лайф&raquo; Полетаев В.А.</p>
+        </div>
+    </div>
+    <div class="hr_block">
+        <div class="center_sidebar">
+            <p>Как мы работаем? Быстро и максимально удобно для вас!</p>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Звоните или оставляйте заявку на сайте</p>
+            </div>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Мы перезваниваем вам в течении 2-х часов</p>
+            </div>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Встречаемся лично, что исключает многие неприятные ситуации</p>
+            </div>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Определяем репертуар по вашему личному заказу</p>
+            </div>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Готовим праздник</p>
+            </div>
+            <div class="hr_part">
+                <img src="/img/6/logo.png" alt="логотип">
+                <p>Наше душевное выступление и ваши позитивные эмоции</p>
+            </div>
+        </div>
+    </div>
+    <div class="center_sidebar">
+        <h3>Мы провели более 300 частных и корпоративных мероприятий, выступлений на городских площадках и
+            концертах</h3>
+        <div class="video_container">
+            <iframe width="420" height="315" src="https://www.youtube.com/embed/HIiFLJT3aoA" frameborder="0"
+                    allowfullscreen></iframe>
+            <iframe width="420" height="315" src="https://www.youtube.com/embed/HIiFLJT3aoA" frameborder="0"
+                    allowfullscreen></iframe>
+        </div>
+        <div class="previews_container">
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+            <a href="#"><img src="/img/6/logo.png" alt="логотип"></a>
+        </div>
+        <form class="sidebar_blue" action="/">
+            <p>Отправьте заявку сейчас и получите скидку 20% на заказ музыкантов</p>
+            <p class="text_blue">До конца акции:</p>
+            <input type="text" name="name" value="" placeholder="Ваше имя*">
+            <input type="tel" name="tel" value="" placeholder="Ваш телефон*">
+            <input type="submit" value="ОСТАВИТЬ ЗАЯВКУ">
+        </form>
+    </div>
 </main>
 
 <footer>
-    Подвал сайта, Копирайты
+    <p>Остались вопросы? Позвоните нам <span>+7(495)760-96-50 +7(495)586-80-03</span></p>
+    <p>Или <a href="#">закажите обратный звонок</a></p>
+    <p class="copyright">&copy; Праздничное агентство &laquo;Vatel&#187; &raquo; музыкальный колектив на праздник в
+        Москве и МО</p>
 </footer>
 
+<div id="order_call">
+    <form action="/">
+        <p>Отправьте заявку и мы обязательно Вам перезвоним</p>
+        Ваше имя* <input type="text" name="name" value="">
+        Ваш телефон* <input type="tel" name="tel" value="">
+        <input type="submit" value="ОСТАВИТЬ ЗАЯВКУ">
+    </form>
+    <div class="senks">
+        <p class="h3">Спасибо за заявку!</p>
+        <p>Наш менеджер перезвонит Вам в ближайшее время</p>
+    </div>
+</div>
+
+
+<!-- FlexSlider -->
+<script defer src="/js/jquery.flexslider-min.js"></script>
+<script>
+    // Can also be used with $(document).ready()
+    $(window).load(function () {
+        $('.flexslider').flexslider({
+            animation: "slide"
+        });
+    });
+</script>
 </body>
 </html>
+
+<!--<div class="mini menu" onclick="$('#menu').toggle('slow');">=</div>-->
+<!--div.header>ul>li*5 далее ажимаем TAB-->
+
+<!--
+<html prefix="og: http://ogp.me/ns/#
+          profile: http://ogp.me/ns/profile#">
+<!--Указание используемых префиксов
+<head>
+    <meta property="og:title" content="Установка VirtualBox + Ubuntu + Apache + MySQL + PHP 7.0 + PhpMyAdmin">
+    <meta property="og:type" content="video.other">
+    <meta property="og:url" content="school-php.com/"><!-- урл страницы
+    <meta property="og:video" content="https://youtu.be/HIiFLJT3aoA">
+    <meta property="og:video:width" content="420">
+    <meta property="og:video:height" content="315">
+    <meta property="og:restrictions:country:allowed string" content="Ru, Ua, Uk">
+    <meta property="og:image" content="http://ic.pics.livejournal.com/ansy_ua/32345376/7581/7581_original.jpg">
+    <meta property="og:description"
+          content="Видео урок для Веб-разработчиков. Подробный урок по установке PHP7 (install php 7) на windows или Ubuntu, затронуты: виртуальная среда VirtualBox + Ubuntu и полный набор для программирования - Apache, MySQL, PHP, PhpMyAdmin">
+    <meta property="og:image" content="http://ic.pics.livejournal.com/ansy_ua/32345376/7581/7581_original.jpg">
+    <meta property="og:site_name" content="school-php.com">
+</head>
+
+<body>
+<div id="video-schema" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+    <a itemprop="url" href="https://youtu.be/HIiFLJT3aoA">
+        <h1 itemprop="name">Установка VirtualBox + Ubuntu + Apache + MySQL + PHP 7.0 + PhpMyAdmin</h1></a>
+    <img itemprop="contentUrl" src="http://ic.pics.livejournal.com/ansy_ua/32345376/7581/7581_original.jpg"
+         width="420" height="315">
+    <p>Здравствуйте, рад видеть Вас на сайте онлайн школы програмирования PHP и HTML. Сегодня мы установим шаг за шагом
+        веб-сервер Apache 2 на Windows. Устанавливать будем через вертуальную среду VirtualBox на Ubuntu, так что этот
+        мануал (руководство по установке) так же подойдет для пользователей работающих с Убунту. Особенно хотелось бы
+        отметить только что вышедшую версию PHP7.0 о которой пойдет речь в этом видео.
+    </p>
+
+    <p itemprop="description">Видео урок рассчитан на Веб-разработчиков и содержит подробные шаги по установке PHP7
+        (install php 7) на windows и Ubuntu, затронуты: виртуальная среда VirtualBox + Ubuntu и полный набор для
+        программирования - Apache, MySQL, PHP, PhpMyAdmin</p>
+
+    <p>После просмотре предоставленного мной учебного видео, Вы легко научитесь самостоятельно подготовить
+        профессиональную среду разработки на домашнем компьютере</p>
+
+    <meta itemprop="duration" content="T6M34S"><!-- Продолжительность видео в формате ISO 8601
+    <meta itemprop="isFamilyFriendly" content="true">
+    <p>Дата загрузки: <span itemprop="uploadDate"> 2013-06-05 </span></p>
+            <span itemprop="thumbnail" itemscope itemtype="http://schema.org/ImageObject"><!--описание изображения предпросмотра
+
+            <meta itemprop="width" content="420">
+            <meta itemprop="height" content="315">
+            </span>
+
+    <iframe width="420" height="315" src="https://www.youtube.com/embed/HIiFLJT3aoA" frameborder="0"
+            allowfullscreen></iframe>
+</div>
+</body>
+
+</html>
+-->
